af1ce4eced9105a975cf371acb3fb9c09b32ca60
zisezicai <liuzezi@qq.com>
2026-06-16 12:59:50 +0800

    清空仓库，准备上传解决方案

--- staticInit.cpp
+++
@@ -1,8 +0,0 @@
-#include "SaveManager.hpp"
-#include "Code.hpp"
-std::unordered_map<std::string, SaveManager> SaveManager::alreadyLoads;
-std::string SaveManager::logPath = "log.txt";
-std::string SaveManager::savePath = "saves";
-SaveManager GameScene::player;
-std::string SetManager::setPath = "set.txt";
-std::map<std::string, std::string> SetManager::settings;
